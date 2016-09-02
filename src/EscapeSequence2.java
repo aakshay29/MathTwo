9dd05f7defbf93535149d11e57a7c92b729b2970
aakshay29 <aakshay.29@gmail.com>
2016-09-02 15:22:36 -0400

    sixth commit

---
+++ src/EscapeSequence2.java
@@ -0,0 +1,6 @@
+public class EscapeSequence2 {
+	public static void main( String[] args ) {
+		System.out.println("Cost\tQuantity\tTotal\n$1,000.00\t4\t$4,000.00\n$50.00\t8\t$400.00\n______________________________\nTOTAL:\t12\t$4,400.00");
+    }
+
+}
