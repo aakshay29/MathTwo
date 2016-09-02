ac0fb69dcdf0b4bd403fc8a7d50d720093be1ede
aakshay29 <aakshay.29@gmail.com>
2016-09-02 15:18:02 -0400

    sixth commit

--- src/Scanners.java
+++ src/Scanners.java
@@ -2,23 +2,19 @@ import java.util.Scanner;
 public class Scanners {
 	public static void main(String[] args) {
 		System.out.println("Enter First name:");
-		Scanner firstInitial = new Scanner(System.in); 
-		String firstInitialt = firstInitial.next();
+		Scanner keyboard = new Scanner(System.in); 
+		String firstInitialt = keyboard.next();
 		System.out.println("Enter Last name:");
-		Scanner lastName = new Scanner(System.in); 
-		String lastNamet = lastName.next();
+		String lastNamet = keyboard.next();
 		System.out.println("Enter House number:");
-		Scanner houseNumber = new Scanner(System.in); 
-		String houseNumbert = houseNumber.next();
+		String houseNumbert = keyboard.next();
 		System.out.println("Enter Street name:");
-		Scanner strtname = new Scanner(System.in); 
-		String strtnamet = strtname.next();
+		String strtnamet = keyboard.next();
 		System.out.println("Enter Street Type:");
-		Scanner strttype = new Scanner(System.in); 
-		String strttypet = strttype.next();
+		String strttypet = keyboard.next();
 		System.out.println("Enter City:");
-		Scanner city = new Scanner(System.in); 
-		String cityt = city.next();
+		String cityt = keyboard.next();
+		keyboard.close();
 		System.out.print(firstInitialt + " " + lastNamet + " " + houseNumbert + " "); 
 		System.out.println(strtnamet + " " + strttypet + " " + cityt);
 	}
