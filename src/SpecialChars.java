126c3f69915eca89108494b67e189b9c2d60970a
aakshay29 <aakshay.29@gmail.com>
2016-09-02 15:30:41 -0400

    seventh commit

---
+++ src/SpecialChars.java
@@ -0,0 +1,10 @@
+public class SpecialChars {
+	public static void main(String[] args){
+	    String message1, message2;
+	    
+	    message1 = "message1 = \\/\\/\\/\\/\\/\r\t\b";
+	    message2 = "\"";
+	    
+	    System.out.println(message1 + message2);
+	}         
+}
