9b2fdfc5e3bf2aaab262f9a6fa0f8ba979b7286d
aakshay29 <aakshay.29@gmail.com>
2016-09-02 16:51:59 -0400

    seventh commit

--- src/AskingQuestions.java
+++ src/AskingQuestions.java
@@ -6,30 +6,23 @@ public class AskingQuestions
   { 
     Scanner keyboard = new Scanner(System.in);
 
-    double num1, num2, num3;
+    int length, width, inches;
+    double water;
 
-    System.out.print( "First integer? " );
-    num1 = keyboard.nextInt();
+    System.out.print( "Enter width of roof" );
+    width = keyboard.nextInt();
 
-    System.out.print( "Second integer? " );
-    num2 = keyboard.nextInt();
-
-    System.out.print( "Third integer? " );
-    num3 = keyboard.nextDouble();
-    System.out.println("The total is : " + (num1 + num2 + num3));
+    System.out.print( "Enter length of roof" );
+    length = keyboard.nextInt();
     
-    //Scanner keyboard = new Scanner(System.in);
+    System.out.print( "Enter cubic inches of water" );
+    inches = keyboard.nextInt();
+    
+    
+    
+    water = (((width/39.37)*(length/39.37)*inches)/231);
 
-    //int num1, num2, num3;
-
-    System.out.print( "First temperature? " );
-    num1 = keyboard.nextInt();
-
-    System.out.print( "Second temperature? " );
-    num2 = keyboard.nextInt();
-
-    System.out.println("The max value is : " + Math.max(num1, num2));
-    System.out.println("The avg value is : " + (num1+num2)/2);
+    System.out.println("The water flow is : " + water);
   }
   
 }
