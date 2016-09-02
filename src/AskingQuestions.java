8f8771b523cc829227324eb5a757b3a73dff7618
aakshay29 <aakshay.29@gmail.com>
2016-09-02 16:42:57 -0400

    seventh commit

--- src/AskingQuestions.java
+++ src/AskingQuestions.java
@@ -17,6 +17,22 @@ public class AskingQuestions
     System.out.print( "Third integer? " );
     num3 = keyboard.nextDouble();
     System.out.println("The total is : " + (num1 + num2 + num3));
+    
+    //Scanner keyboard = new Scanner(System.in);
+
+    //int num1, num2, num3;
+
+    System.out.print( "First temperature? " );
+    num1 = keyboard.nextInt();
+
+    System.out.print( "Second temperature? " );
+    num2 = keyboard.nextInt();
+
+    System.out.println("The max value is : " + Math.max(num1, num2));
+    System.out.println("The avg value is : " + (num1+num2)/2);
   }
   
 }
+
+
+
